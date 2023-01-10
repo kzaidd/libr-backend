7280322445862f5fd4e95f27a077a2e8c2ffac97
Zaid Khan <53516135+kzaidd@users.noreply.github.com>
2023-01-10 19:04:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ app.use(cors())
 
 app.use('/api', postRoute);
 
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "./client/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-}
-    
-
-    
 const PORT = process.env.PORT || 50000;
 
 app.listen(PORT, () => {
